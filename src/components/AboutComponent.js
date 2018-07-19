aa1536c81cdf16f384486b5449422c97beecb40a
sandakelum tharindu <tharooshawn@gmail.com>
2018-07-19 12:46:41 +0530

    about navigation

---
+++ src/components/AboutComponent.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Media } from 'reactstrap';
+
+const RenderLeader = (() => {
+    return (
+        <Media>
+        <Media left href="#">
+          <Media object data-src="holder.js/64x64" alt="Generic placeholder image" />
+        </Media>
+        <Media body>
+          <Media heading>
+            Media heading
+          </Media>
+          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+        </Media>
+      </Media>
+    );
+})
+
+const About = (() => {
+return (
+    <div className="container">
+        <RenderLeader />
+    </div>
+);
+})
+
+export default About;
