9b761317a8928a50bb24173ef02f43980fe1272f
sandakelum tharindu <tharooshawn@gmail.com>
2018-07-19 15:10:16 +0530

    display the list of leaders assign 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -50,7 +50,7 @@ class Main extends Component {
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
                     <Route path="/menu/:dishId" component={DishWithId} />
                     <Route exact path="/contactus" component={Contact} />
-                    <Route exact path="/aboutus" component={About} />
+                    <Route path="/aboutus" component={() => <About leaders={this.state.leaders}/>} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
