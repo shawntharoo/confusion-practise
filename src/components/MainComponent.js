25b473bf5c97a0fa31f8cb0363d1153e06e00f65
shawntharoo <tharooshawn@gmail.com>
2018-07-19 07:50:25 +0530

    Single page application

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,7 +4,11 @@ import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
     constructor(props) {
@@ -12,6 +16,9 @@ class Main extends Component {
 
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotions : PROMOTIONS,
+            leaders : LEADERS
         };
     }
 
@@ -19,7 +26,10 @@ class Main extends Component {
     render() {
         const HomePage = () => {
             return (
-                <Home />
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
             )
         }
         return (
@@ -28,6 +38,7 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -1,125 +1,43 @@
-export const DISHES = [{
-    id: 0,
-    name: 'Uthappizza',
-    image: 'assets/images/uthappizza.png',
-    category: 'mains',
-    label: 'Hot',
-    price: '4.99',
-    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-    comments: [
+export const DISHES =
+    [
         {
-            comment : "Uthappizza",
-            author: "john",
-            id: 1
+        id: 0,
+        name:'Uthappizza',
+        image: '/assets/images/uthappizza.png',
+        category: 'mains',
+        label:'Hot',
+        price:'4.99',
+        featured: true,
+        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                    
         },
         {
-            comment : "Uthappizza",
-            author: "john",
-            id: 2
+        id: 1,
+        name:'Zucchipakoda',
+        image: '/assets/images/zucchipakoda.png',
+        category: 'appetizer',
+        label:'',
+        price:'1.99',
+        featured: false,
+        description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
         },
         {
-            comment : "Uthappizza",
-            author: "john",
-            id: 3
+        id: 2,
+        name:'Vadonut',
+        image: '/assets/images/vadonut.png',
+        category: 'appetizer',
+        label:'New',
+        price:'1.99',
+        featured: false,
+        description:'A quintessential ConFusion experience, is it a vada or is it a donut?'
         },
         {
-            comment : "Uthappizza",
-            author: "john",
-            id: 4
+        id: 3,
+        name:'ElaiCheese Cake',
+        image: '/assets/images/elaicheesecake.png',
+        category: 'dessert',
+        label:'',
+        price:'2.99',
+        featured: false,
+        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
         }
-    ]
-},
-{
-    id: 1,
-    name: 'Zucchipakoda',
-    image: 'assets/images/zucchipakoda.png',
-    category: 'appetizer',
-    label: '',
-    price: '1.99',
-    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-    comments: [
-        {
-            comment : "zucchipakoda",
-            author: "robert",
-            id: 1
-        },
-        {
-            comment : "zucchipakoda",
-            author: "robert",
-            id: 2
-        },
-        {
-            comment : "zucchipakoda",
-            author: "robert",
-            id: 3
-        },
-        {
-            comment : "zucchipakoda",
-            author: "robert",
-            id: 4
-        }
-    ]
-},
-{
-    id: 2,
-    name: 'Vadonut',
-    image: 'assets/images/vadonut.png',
-    category: 'appetizer',
-    label: 'New',
-    price: '1.99',
-    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?',
-    comments: [
-        {
-            comment : "Vadonut",
-            author: "roshell",
-            id: 1
-        },
-        {
-            comment : "Vadonut",
-            author: "roshell",
-            id: 2
-        },
-        {
-            comment : "Vadonut",
-            author: "roshell",
-            id: 3
-        },
-        {
-            comment : "Vadonut",
-            author: "roshell",
-            id: 4
-        }
-    ]
-},
-{
-    id: 3,
-    name: 'ElaiCheese Cake',
-    image: 'assets/images/elaicheesecake.png',
-    category: 'dessert',
-    label: '',
-    price: '2.99',
-    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-    comments: [
-        {
-            comment : "ElaiCheese",
-            author: "chirst",
-            id: 1
-        },
-        {
-            comment : "ElaiCheese",
-            author: "chirst",
-            id: 2
-        },
-        {
-            comment : "ElaiCheese",
-            author: "chirst",
-            id: 3
-        },
-        {
-            comment : "ElaiCheese",
-            author: "chirst",
-            id: 4
-        }
-    ]
-}
-]
+    ];
