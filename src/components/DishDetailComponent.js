8d7421f327d2cbbcff708016b5ba252197232714
shawntharoo <tharooshawn@gmail.com>
2018-07-27 07:47:55 +0530

    retrieve data from json-server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import CommentForm from './CommentFormComponent';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderDish({ dish, comments, addComment }) {
     if (dish != null) {
@@ -10,7 +11,7 @@ function RenderDish({ dish, comments, addComment }) {
             <div className="row">
                 <div className="col-12 col-md-5 m-1">
                     <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
                             <CardTitle> {dish.name}</CardTitle>
                             <CardText>{dish.description}</CardText>
